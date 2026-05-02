c506a243415b1203471854af7d08cdfac2cf5cec
Cabragamer05 <sebastiancabreragomez@gmail.com>
2026-05-02 11:25:55 -0400

    Prueba 2
    ojala mrd

--- Frontend/src/services/mobileScanService.js
+++ Frontend/src/services/mobileScanService.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const API_URL = import.meta.env.VITE_API_URL || "https://unluckily-headgear-limb.ngrok-free.dev";
 
 async function uploadFile(endpoint, file, extraData = {}) {
   try {
